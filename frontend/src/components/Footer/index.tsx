1113f73e2d48c6c264863f6619766cb118c522b2
Fernanda <fernandamaia1001@gmail.com>
2021-05-04 01:09:40 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/ferpalma" target="_blank" rel="noreferrer">Fernanda Palma</a></p>
+                <p className="text-light"><small><strong>Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
