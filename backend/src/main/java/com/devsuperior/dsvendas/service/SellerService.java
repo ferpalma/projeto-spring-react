b2d2bd791266fbe8cdf4fee17fe2838cf02b83f7
Fernanda <fernandamaia1001@gmail.com>
2021-05-05 12:48:55 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.hibernate.loader.plan.build.internal.returns.CollectionFetchableElementCompositeGraph;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsvendas.dto.SellerDTO;
+import com.devsuperior.dsvendas.entities.Seller;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+	// Injeta a instancia automaticamente pelo framework, nova, new, 
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result =  repository.findAll();
+		
+		// A função map vai converter uma coleção original em uma nova coleção de outro tipo que no caso seria lista de SellerDTO
+		// collect é para converter de novo para lista
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
