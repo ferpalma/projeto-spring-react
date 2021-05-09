58961c6217464ea05ad523baf66f5d42fac78a12
Fernanda <fernandamaia1001@gmail.com>
2021-05-09 02:32:15 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+// ?? operador de valor nulo, ele pega a variável definida no Netlify REACT_APP_BACKEND_URL, se
+// não estiver definida ele pega a URL local - 'http://localhost:8080'
